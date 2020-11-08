93bf256035c66f33ae33c48b0c4421b9fddc56f8
DiEr <yeraskz@gmail.com>
2020-11-08 18:10:15 +0600

    PgConfiguration

--- api-gateway/src/main/java/kz/med/configuration/ConfigurationPostgres.java
+++ api-gateway/src/main/java/kz/med/configuration/ConfigurationPostgres.java
@@ -1,82 +1,82 @@
-package kz.med.configuration;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.stream.Collectors;
-
-@Configuration
-@EnableTransactionManagement
-@EnableAutoConfiguration
-@EnableJpaRepositories(
-        entityManagerFactoryRef = "EntityManager",
-        transactionManagerRef = "TransactionManager",
-        basePackages = {
-                "kz.med"
-        }
-)
-public class ConfigurationPostgres {
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource DataSource() {
-        return DataSourceBuilder
-                .create()
-                .build();
-    }
-
-    @Primary
-    @Bean(name = "EntityManager")
-    public LocalContainerEntityManagerFactoryBean EntityManagerFactory(EntityManagerFactoryBuilder builder) {
-        return builder
-                .dataSource(DataSource())
-                .properties(hibernateProperties())
-                .packages(
-                        "kz.med.model"
-                )
-                .persistenceUnit("dbPU")
-                .build();
-    }
-
-    @Primary
-    @Bean(name = "TransactionManager")
-    public PlatformTransactionManager TransactionManager(@Qualifier("EntityManager") EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
-
-    private Map<String, Object> hibernateProperties() {
-
-        Resource resource = new ClassPathResource("hibernate.properties");
-        try {
-            Properties properties = PropertiesLoaderUtils.loadProperties(resource);
-            return properties.entrySet().stream()
-                    .collect(Collectors.toMap(
-                            e -> e.getKey().toString(),
-                            e -> e.getValue())
-                    );
-        } catch (IOException e) {
-            return new HashMap<String, Object>();
-        }
-    }
-
-}
+//package kz.med.configuration;
+//
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.support.PropertiesLoaderUtils;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.persistence.EntityManagerFactory;
+//import javax.sql.DataSource;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.Properties;
+//import java.util.stream.Collectors;
+//
+//@Configuration
+//@EnableTransactionManagement
+//@EnableAutoConfiguration
+//@EnableJpaRepositories(
+//        entityManagerFactoryRef = "EntityManager",
+//        transactionManagerRef = "TransactionManager",
+//        basePackages = {
+//                "kz.med"
+//        }
+//)
+//public class ConfigurationPostgres {
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource DataSource() {
+//        return DataSourceBuilder
+//                .create()
+//                .build();
+//    }
+//
+//    @Primary
+//    @Bean(name = "EntityManager")
+//    public LocalContainerEntityManagerFactoryBean EntityManagerFactory(EntityManagerFactoryBuilder builder) {
+//        return builder
+//                .dataSource(DataSource())
+//                .properties(hibernateProperties())
+//                .packages(
+//                        "kz.med.model"
+//                )
+//                .persistenceUnit("dbPU")
+//                .build();
+//    }
+//
+//    @Primary
+//    @Bean(name = "TransactionManager")
+//    public PlatformTransactionManager TransactionManager(@Qualifier("EntityManager") EntityManagerFactory entityManagerFactory) {
+//        return new JpaTransactionManager(entityManagerFactory);
+//    }
+//
+//    private Map<String, Object> hibernateProperties() {
+//
+//        Resource resource = new ClassPathResource("hibernate.properties");
+//        try {
+//            Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+//            return properties.entrySet().stream()
+//                    .collect(Collectors.toMap(
+//                            e -> e.getKey().toString(),
+//                            e -> e.getValue())
+//                    );
+//        } catch (IOException e) {
+//            return new HashMap<String, Object>();
+//        }
+//    }
+//
+//}
