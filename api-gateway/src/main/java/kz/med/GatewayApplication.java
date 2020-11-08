148275673635e9d47c9b21fbd5a4102877212f24
DiEr <yeraskz@gmail.com>
2020-11-08 17:24:50 +0600

    PgConfiguration

--- api-gateway/src/main/java/kz/med/GatewayApplication.java
+++ api-gateway/src/main/java/kz/med/GatewayApplication.java
@@ -2,8 +2,14 @@ package kz.med;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@CrossOrigin(origins = "http://localhost:4200")
 public class GatewayApplication {
 
     public static void main(String[] args) {
